a4c3907cd4b91a8bd74bb786e900bf4e12d49906
kangning cui <kangnicui2@gmail.com>
2023-09-08 12:02:16 +0800

    Update full_test.py

--- full_test.py
+++ full_test.py
@@ -1,4 +1,4 @@
-from PIL import Image
+from PIL import Image, ImageOps
 import numpy as np
 import os
 import re
@@ -18,16 +18,18 @@ import math
 import cv2
 
 path = "/deac/csc/paucaGrp/student-research-team/palm_detection/"  # Change this line to set your custom path /deac/csc/paucaGrp/student-research-team/palm_detection/
-
-nnodes = 128
+nnodes = 64
+save_path = os.path.join(path, f"figures_{nnodes}_large_test/") 
 
 # Predicts the large images
-img_counter=30
-patch_size = (40, 40)
+stride = 10
+patch_size = (100, 100)
 small_image_size = (5000, 5000)
 Image.MAX_IMAGE_PIXELS = 1000000000
-large_image_path = f"{path}Palm/FCAT2APPK_new.jpg"
-large_image = Image.open(large_image_path)
+small_image = Image.open(f"{path}Palm/FCAT2APPK_new.jpg")
+small_image = ImageOps.expand(small_image, (0, 0, 5, 0), fill=0)
+small_heatmap_array = []
+small_heatmap_array_prob = []
 
 def initialize_model():
     model = models.resnet18(pretrained=False)
@@ -57,125 +59,97 @@ def preprocess_test(patch_img):
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 model = initialize_model()
-model.load_state_dict(torch.load(f'{path}figures_{nnodes}_updated/resnet18_model.pth'))
+model.load_state_dict(torch.load(f'{path}figures_{nnodes}_large/resnet18_model.pth'))
 model = model.to(device)
 model = model.eval()
 
-initial_image_number = 0
-stride = 10
+accumulator = np.zeros((small_image.height // stride, small_image.width // stride))
+counter = np.ones((small_image.height // stride, small_image.width // stride))
 
-for i in range(initial_image_number, img_counter):
-    small_image = Image.open(f"{path}Palm/small_images_new/small_image_{i}.jpg")
-    small_heatmap_array = []
-    small_heatmap_array_prob = []
-    
-    # Initialize the accumulator and counter arrays
-    accumulator = np.zeros((small_image.height // stride, small_image.width // stride))
-    counter = np.ones((small_image.height // stride, small_image.width // stride))
+# Iterate over the small image to get the patches
+for x in range(0, small_image.width, stride):
+    for y in range(0, small_image.height, stride):
+        # Get a patch from the small image
+        patch = small_image.crop((x, y, x + patch_size[0], y + patch_size[1]))
 
-    # Iterate over the small image to get the patches
-    for x in range(0, small_image.width, stride):
-        for y in range(0, small_image.height, stride):
-            # Get a patch from the small image
-            patch = small_image.crop((x, y, x + patch_size[0], y + patch_size[1]))
+        # Convert patch to numpy array
+        patch_array = np.array(patch)
 
-            # Convert patch to numpy array
-            patch_array = np.array(patch)
+        # Check if patch contains 25% or more black or white pixels and label it as 'non palm'
+        white_pixels = np.sum(patch_array > 200) / 3  # divide by 3 to account for RGB channels
+        black_pixels = np.sum(patch_array <= 50) / 3
+        total_pixels = patch_size[0] * patch_size[1]
 
-            # Check if patch contains 40% or more black or white pixels and label it as 'non palm'
-            white_pixels = np.sum(patch_array > 200) / 3  # divide by 3 to account for RGB channels
-            black_pixels = np.sum(patch_array <= 50) / 3
-            total_pixels = patch_size[0] * patch_size[1]
+        if (white_pixels + black_pixels) / total_pixels >= 0.25:
+            small_heatmap_array.append(0)  # non palm
+            small_heatmap_array_prob.append(0)
+            continue
 
-            if (white_pixels + black_pixels) / total_pixels >= 0.4:
-                small_heatmap_array.append(0)  # non palm
-                small_heatmap_array_prob.append(0)
-                continue
+        # If not 'non palm', extract features using ResNet18 and predict
+        patch = patch.resize((100, 100))
+        patch_img = Image.fromarray(patch_array.astype('uint8'), 'RGB')
 
-            # If not 'non palm', extract features using ResNet18 and predict
-            patch = patch.resize((40, 40))
-            patch_img = Image.fromarray(patch_array.astype('uint8'), 'RGB')
+        # Preprocess the patch
+        patch_t = preprocess_test(patch_img)
+        patch_u = torch.unsqueeze(patch_t, 0)
+        patch_u = patch_u.to(device)  # Remember to move your inputs to the same device as your model
 
-            # Preprocess the patch
-            patch_t = preprocess_test(patch_img)
-            patch_u = torch.unsqueeze(patch_t, 0)
-            patch_u = patch_u.to(device)  # Remember to move your inputs to the same device as your model
+        # Extract features
+        with torch.no_grad():
+            output = model(patch_u)
 
-            # Extract features
-            with torch.no_grad():
-                output = model(patch_u)
+        # Apply softmax to get probabilities and find the class with maximum probability
+        probabilities = torch.exp(output).cpu()
+        
+        # Add the prediction to the accumulator and increment the counter for all overlapping patches
+        for dx in range(-1, 2):
+            for dy in range(-1, 2):
+                if 0 <= x // stride + dx < accumulator.shape[1] and 0 <= y // stride + dy < accumulator.shape[0]:
+                    accumulator[y // stride + dy, x // stride + dx] += probabilities[0][1].item()
+                    counter[y // stride + dy, x // stride + dx] += 1
 
-            # Apply softmax to get probabilities and find the class with maximum probability
-            probabilities = torch.exp(output).cpu()
-            
-            # Add the prediction to the accumulator and increment the counter for all overlapping patches
-            for dx in range(-1, 2):
-                for dy in range(-1, 2):
-                    if 0 <= x // stride + dx < accumulator.shape[1] and 0 <= y // stride + dy < accumulator.shape[0]:
-                        accumulator[y // stride + dy, x // stride + dx] += probabilities[0][1].item()
-                        counter[y // stride + dy, x // stride + dx] += 1
+# After all predictions have been made, divide the accumulator by the counter to get the average predictions
+average_predictions = accumulator / counter
 
-        # After all predictions have been made, divide the accumulator by the counter to get the average predictions
-    average_predictions = accumulator / counter
+# Reshape the average_predictions to the shape of the small image
+average_predictions = average_predictions.reshape((small_image.height // stride, small_image.width // stride))
+print(np.shape(average_predictions))
 
-    # Reshape the average_predictions to the shape of the small image
-    average_predictions = average_predictions.reshape((small_image.height // stride, small_image.width // stride))
-    print(np.shape(average_predictions))
+# Repeat average_predictions 10 times along x and y axes
+average_predictions_repeated = np.repeat(np.repeat(average_predictions, 10, axis=0), 10, axis=1)
 
-    # Convert PIL Image to numpy array
-    small_image_np = np.array(small_image)
-    
-    # Compute the scaling factors
-    scale_y = small_image_np.shape[0] / average_predictions.shape[0]
-    scale_x = small_image_np.shape[1] / average_predictions.shape[1]
+# Pad average_predictions_cropped to match small_image dimensions
+rows_to_pad = small_image.height - average_predictions_repeated.shape[0]
+cols_to_pad = small_image.width - average_predictions_repeated.shape[1]
+average_predictions_padded = np.pad(average_predictions_repeated, ((0, rows_to_pad), (0, cols_to_pad)), 'constant', constant_values=0)
 
-    # Resize the heatmap
-    average_predictions_resized = np.repeat(np.repeat(average_predictions, stride, axis=0), stride, axis=1)
-    
-    # Save the heatmap array and the corresponding figure
-    np.save(f"{path}figures_{nnodes}_updated/small_image_{i}_heatmap_prob.npy", average_predictions_resized)
+# Print dimensions for debugging
+print(f"Dimensions after repeating, cropping and padding: {average_predictions_padded.shape}")
 
-    # Convert to float32 before normalize
-    average_predictions_resized = average_predictions_resized.astype(np.float32)
+# Convert to float32 before normalize
+average_predictions_padded = average_predictions_padded.astype(np.float32)
 
-    # Convert the probability heatmap to a mask
-    mask_prob = (average_predictions_resized * 255).astype(np.uint8)
+# Convert the probability heatmap to a mask
+mask_prob = (average_predictions_padded * 255).astype(np.uint8)
 
-    # Convert PIL Image to numpy array
-    small_image = np.array(small_image)
+# Convert PIL Image to numpy array
+small_image_np = np.array(small_image)
 
-    # Highlight the palms in the image using the probability mask
-    highlighted_image_prob = cv2.addWeighted(small_image, 0.5, cv2.cvtColor(mask_prob, cv2.COLOR_GRAY2BGR), 0.5, 0)
+# Print dimensions for debugging
+print(f"Dimensions of small_image: {small_image_np.shape}")
+print(f"Dimensions of mask_prob before color conversion: {mask_prob.shape}")
 
-    # Save the probability highlighted image
-    cv2.imwrite(f"{path}figures_{nnodes}_updated/small_image_{i}_highlighted_prob.jpg", highlighted_image_prob)
+# Convert the grayscale mask_prob to BGR
+mask_prob_bgr = cv2.cvtColor(mask_prob, cv2.COLOR_GRAY2BGR)
 
-# Create a new image with a slightly larger size
-new_image_width, new_image_height = 30000, 25000
-large_heatmap_prob = np.zeros((new_image_height, new_image_width))
-large_image_highlighted_prob = Image.new('RGB', (new_image_width, new_image_height))
+# Print dimensions after color conversion for debugging
+print(f"Dimensions of mask_prob after color conversion: {mask_prob_bgr.shape}")
 
-# Specify the number of small images in each dimension
-num_images_width = new_image_width // small_image_size[0]
-num_images_height = new_image_height // small_image_size[1]
+# Make sure both arrays have the same dimensions before calling cv2.addWeighted
+if small_image_np.shape == mask_prob_bgr.shape:
+    highlighted_image_prob = cv2.addWeighted(small_image_np, 0.5, mask_prob_bgr, 0.5, 0)
+else:
+    print("The dimensions do not match. Cannot perform addWeighted.")
 
-for i in range(num_images_width):
-    for j in range(num_images_height):
-        # Place each small image onto the large image canvas in the right order
-        small_image_highlighted_prob = Image.open(f"{path}figures_{nnodes}_updated/small_image_{i*num_images_height + j}_highlighted_prob.jpg")
-        large_image_highlighted_prob.paste(small_image_highlighted_prob, (i*small_image_size[0], j*small_image_size[1]))
-        # Place the small heatmap onto the large heatmap canvas
-        small_heatmap_prob = np.load(f"{path}figures_{nnodes}_updated/small_image_{i*num_images_height + j}_heatmap_prob.npy")
-        large_heatmap_prob[j*small_image_size[1]:(j+1)*small_image_size[1], i*small_image_size[0]:(i+1)*small_image_size[0]] = small_heatmap_prob
-
-# Crop the combined image to the original image size
-large_image_highlighted_prob = large_image_highlighted_prob.crop((0, 0, large_image.width, large_image.height))
-
-# Crop the combined heatmap to the original image size
-large_heatmap_prob = large_heatmap_prob[0:large_image.height, 0:large_image.width]
-
-# Save the resulting large image
-large_image_highlighted_prob.save(f"{path}figures_{nnodes}_updated/FCAT2APPK_highlighted_prob.jpg")
-
-# Save the resulting large heatmap
-np.save(f"{path}figures_{nnodes}_updated/FCAT2APPK_heatmap_prob.npy", large_heatmap_prob)
+# Save the probability highlighted image
+cv2.imwrite(f"{save_path}FCAT2APPK_highlighted_prob.jpg", highlighted_image_prob)
